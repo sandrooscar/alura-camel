ab9469f4701a38b60be3f18a198225de2ebe2ccb
Taquara <prof.sandrob@gmail.com>
2021-02-14 07:12:36 -0300

    aula03-conectando_endpoints_http - parametros para métodos - query params

--- src/main/java/br/com/caelum/camel/RotaPedidos.java
+++ src/main/java/br/com/caelum/camel/RotaPedidos.java
@@ -17,17 +17,21 @@ public class RotaPedidos {
 			public void configure() throws Exception {
 				//monitora a cada 5 segundos, noop=true-> mantêm os arquivos na pasta original
 				from("file:pedidos?delay=5s&noop=true").
+				setProperty("pedidoId", xpath("/pedido/id/text()")).
+				setProperty("clienteId", xpath("/pedido/pagamento/email-titular/text()")).
+				
 				split().
 					xpath("/pedido/itens/item").
 				filter().
 					xpath("/item/formato[text()='EBOOK']").
+					setProperty("ebookId", xpath("/item/livro/codigo/text()")).
 					//imprime o id gerado pelo camel, body-> corpo do arquivo
 					//log("${id} ${body}").
 				marshal().
 					xmljson().
 				log("${body}").
 					setHeader(Exchange.HTTP_METHOD, constant(org.apache.camel.component.http4.HttpMethods.GET)).
-					setHeader(Exchange.HTTP_QUERY, constant("clienteId=breno@abc.com&pedidoId=123&ebookId=ARQ")).
+					setHeader(Exchange.HTTP_QUERY, constant("clienteId=${property.clienteId}&pedidoId=${property.pedidoId}&ebookId=${property.ebookId}")).
 				to("http4://localhost:8080/webservices/ebook/item");
 			}
 			
